f820e68e633e000e05e4871d28b7b2cac7071617
alexsandro <alexsandrosipelli@outlook.com>
2024-07-03 18:00:59 -0300

    començando a estilizar as telas

---
+++ Gerenciamento de Tarefas/src/main/java/Portifolio/Portifolio/controller/ApresentacaoController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Portifolio.Portifolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author alexsandro
+ */
+@Controller
+public class ApresentacaoController {
+
+    @GetMapping("/apresentacao")
+    public String apresentarInformacoes() {
+        return "Apresentacao";
+    }
+}
